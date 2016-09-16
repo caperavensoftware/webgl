4ecb812f5af3e69970447276912686fda93c78e1
caperaven@icloud.com <caperaven@icloud.com>
2016-09-16 22:46:50 +0200

    initial

--- src/views/test1/test1.ts
+++ src/views/test1/test1.ts
@@ -1,11 +1,12 @@
 import {GLCanvas} from "./../../webgl/gl-canvas";
+import {converter} from "./../../webgl/converter";
 
 export class Test1 {
     glCanvas: GLCanvas;
 
     attached() {
         const canvas = document.getElementById("glCanvas");
-        this.glCanvas = new GLCanvas(canvas, {r: 0.5, g: 0.0, b:0.0, a:1});
+        this.glCanvas = new GLCanvas(canvas, converter.rgbToColor({r:255, g:187, b:0, a:255}));
     }
 
     detached() {
